f07e52037614dd3778a18815abbd3000330cafe6
Sai Koushik Ch.V <saikoushik.chundi@gmail.com>
2024-06-18 19:49:11 +0530

    Add files via upload

--- Q1.cpp
+++ Q1.cpp
@@ -1,34 +1,40 @@
 #include <bits/stdc++.h>
 using namespace std;
+bool check(const vector<int>& h,int maxHeight,long long k){
+    long long cost = 0;
+    for(long long h : h){
+        if(h>maxHeight){
+            long long m=maxHeight;
+            cost+=(h*(h+1)/2)-(m*(m+1)/2);
+            if(cost>k) return false;
+        }
+    }
+    return cost<=k;
+}
 int main(){
     int t;
     cin >> t;
-    while(t--){
-        int n,k;
+    while (t--){
+        int n;
+        long long k;
         cin >> n >> k;
-        vector<int> v(n);
+        vector<int> h(n);
         for(int i=0;i<n;i++){
-            cin >> v[i];
+            cin >> h[i];
         }
-        priority_queue<int> maxHeap;
-        for(int i=0;i<n;++i){
-            maxHeap.push(v[i]);
+        int l=0;
+        int r=*max_element(h.begin(), h.end());
+        int res=r;
+        while(l<=r){
+            int mid=l+(r-l)/2;
+            if(check(h,mid,k)){
+                res=mid;
+                r=mid-1;
+            }else{
+                l=mid+1;
+            }
         }
-        while(!maxHeap.empty()&&maxHeap.top()<=k){
-            k-=maxHeap.top();
-            maxHeap.push(maxHeap.top()-1);
-            maxHeap.pop();
-        }
-        cout << maxHeap.top() << "\n";
+        cout << res << endl;
     }
     return 0;
 }
-
-
-
-
-
-
-
-
-
